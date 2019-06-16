e3dfc69e9ec8ca4dd28f1f8b06634d4fddeb0b45
sithon512 <jrlatta512@gmail.com>
2019-06-16 11:18:20 -0400

    Added login/logout urls.

--- DnDPlanner/main/urls.py
+++ DnDPlanner/main/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-	path("", views.homepage, name="homepage"),
+	path('', views.homepage, name='homepage'),
+	path('logout/', views.logout_request, name='logout'),
+	path('login/', views.login_request, name='login'),
 ]
