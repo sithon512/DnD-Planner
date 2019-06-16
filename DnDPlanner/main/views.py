033e9385d2669eaf230c9080da992c5f868663cd
sithon512 <jrlatta512@gmail.com>
2019-06-16 10:30:49 -0400

    Added messages functionality.

--- DnDPlanner/main/views.py
+++ DnDPlanner/main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
+	messages.success(request, 'messages working')
 	return render(	request,
 					"main/homepage.html",
 				)
+
+def login_request(request):
+	pass
+
+def logout_request(request):
+	pass
