e1760915b448e4bb77626b51bf7f8c3b2d317d52
sithon512 <jrlatta512@gmail.com>
2019-06-16 11:18:50 -0400

    Started working on login/logout views.

--- DnDPlanner/main/views.py
+++ DnDPlanner/main/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import logout, login
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
-	messages.success(request, 'messages working')
 	return render(	request,
-					"main/homepage.html",
+					'main/homepage.html',
 				)
 
 def login_request(request):
-	pass
+	form = AuthenticationForm()
+	return render(	request,
+					'main/login.html',
+					{'form': form}
+				)
 
 def logout_request(request):
-	pass
+	logout(request)
+	messages.success(request, 'You have been logged out.')
+	return redirect('main:homepage')
