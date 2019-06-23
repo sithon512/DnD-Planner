2d89b5fbe0d2365b43db8fd39b428dbd8071800f
sithon512 <jrlatta512@gmail.com>
2019-06-23 17:06:17 -0400

    Added view for login screen.

--- DnDPlanner/main/views.py
+++ DnDPlanner/main/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import logout, login
+from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
@@ -10,6 +10,24 @@ def homepage(request):
 				)
 
 def login_request(request):
+	if request.user.is_authenticated:
+		return redirect('main:homepage')
+
+	if request.method == 'POST':
+		form = AuthenticationForm(request, request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.success(request, f"Logged in user: {username}")
+				return redirect("main:homepage")
+			else:
+				messages.error(request, "Invalid username or password")
+		else:
+			messages.error(request, 'Invalid username or password')
+
 	form = AuthenticationForm()
 	return render(	request,
 					'main/login.html',
