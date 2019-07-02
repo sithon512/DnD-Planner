b00b1bd1b12cdf0c1ff481d795da904b08df2ad5
sithon512 <jrlatta512@gmail.com>
2019-07-01 21:58:21 -0400

    Define core models

--- DnDPlanner/main/models.py
+++ DnDPlanner/main/models.py
@@ -2,6 +2,38 @@
 Defines all of the database models.
 """
 
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+# standard models
+class Campaign(models.Model):
+	title = models.CharField(max_length=50, verbose_name='Campaign Title')
+	description = models.TextField(verbose_name='Description')
+	ofk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Location(models.Model):
+	name = models.CharField(max_length=50, verbose_name='Location Name')
+	description = models.TextField(verbose_name='Description')
+	ofk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Encounter(models.Model):
+	name = models.CharField(max_length=50, verbose_name='Encounter Name')
+	description = models.TextField(verbose_name='Description')
+	ofk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Item(models.Model):
+	name = models.CharField(max_length=50, verbose_name='Item Name')
+	description = models.TextField(verbose_name='Description')
+	ofk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Creature(models.Model):
+	name = models.CharField(max_length=50, verbose_name='Creature Name')
+	description = models.TextField(verbose_name='Description')
+	ofk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class PlotMoment(models.Model):
+	name = models.CharField(max_length=50, verbose_name='Plot Moment Name')
+	description = models.TextField(verbose_name='Description')
+	ofk = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+# mapping tables
