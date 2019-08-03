c3b794709b4aa5bf500c4d07ee5e22761491c509
sithon512 <jrlatta512@gmail.com>
2019-08-03 13:02:05 -0400

    Added admin pages for models.

--- DnDPlanner/main/admin.py
+++ DnDPlanner/main/admin.py
@@ -4,5 +4,16 @@ further, how those fields should be represented.
 """
 
 from django.contrib import admin
+from .models import Campaign, Location, Encounter, Item, Creature, PlotMoment, Note, Skill, Tool, WeaponProperty, Map
 
-# Register your models here.
+admin.site.register(Campaign)
+admin.site.register(Location)
+admin.site.register(Encounter)
+admin.site.register(Item)
+admin.site.register(Creature)
+admin.site.register(PlotMoment)
+admin.site.register(Note)
+admin.site.register(Skill)
+admin.site.register(Tool)
+admin.site.register(WeaponProperty)
+admin.site.register(Map)
