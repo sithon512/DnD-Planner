82c4fa1d475ac1f801f8236f6b7b77843937a752
sithon512 <jrlatta512@gmail.com>
2019-08-03 13:03:17 -0400

    Added URL for planner home

--- DnDPlanner/main/urls.py
+++ DnDPlanner/main/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
 	path('logout/', views.logout_request, name='logout'),
 	path('login/', views.login_request, name='login'),
 	path('init-tracker/', views.init_tracker, name='init-tracker'),
+	path('planner-home/', views.planner_home, name='planner-home'),
 ]
