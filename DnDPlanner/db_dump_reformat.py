fd617786c426a6ffbdf761bb699a78a87a77cf78
sithon512 <jrlatta512@gmail.com>
2019-08-08 21:38:33 -0400

    Added python script for converting database dump from UTF-16 to UTF-8.

---
+++ DnDPlanner/db_dump_reformat.py
@@ -0,0 +1,4 @@
+with open('db.json', 'rb') as source_file:
+  with open('new_db.json', 'w+b') as dest_file:
+    contents = source_file.read()
+    dest_file.write(contents.decode('utf-16').encode('utf-8'))
