6a778bca4ee5d241994f980e52675da872f4090c
sithon512 <jrlatta512@gmail.com>
2019-08-08 20:05:48 -0400

    Updated user model metadata and to string.

--- DnDPlanner/users/models.py
+++ DnDPlanner/users/models.py
@@ -5,5 +5,9 @@ from django.db import models
 class DDPUser(AbstractUser):
 	pass # additional fields
 
+	class Meta:
+		db_table = 'ddpuser'
+		verbose_name_plural = 'DDPUsers'
+
 	def __str__(self):
-		return self.email
+		return self.username
